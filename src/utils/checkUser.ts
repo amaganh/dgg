13765e191346cd320431b5fa3b638a89c71681fc
Angel Magan <amagan@immediait.com>
2025-05-06 15:58:29 +0200

    test

--- src/utils/checkUser.ts
+++
@@ -1,8 +0,0 @@
-import UserModel from '../models/User';
-
-export async function checkUser(uuid: string) {
-  const user = await UserModel.findById(uuid);
-  if (!user) return false;
-
-  return user;
-}
